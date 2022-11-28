061acf09c893828e8a888076b4ef151992f635d9
HapH <jyoon0c11@gmail.com>
2022-11-29 03:49:26 +0900

    login앱 생성, mysql 연동

--- Kobot_prj/settings.py
+++ Kobot_prj/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "Kobot_prj.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "Kobot",
+        'USER': 'root',
+        'PASSWORD': 'qq151mw00@@',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
---
+++ my_setting.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "Kobot",
+        'USER': 'root',
+        'PASSWORD': 'qq151mw00@@',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+SECRET_KEY = "django-insecure-dtkjb8l7hgaua4(ercume-zhkjkoa(_4r(!v7_ef7hh)7h+c)&"
