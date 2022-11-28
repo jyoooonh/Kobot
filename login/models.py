2ea751743b6fdcdd178047dc60b46ec6952b4571
HapH <jyoon0c11@gmail.com>
2022-11-29 04:27:14 +0900

    Login 모델 생성

--- login/models.py
+++ login/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Login(models.Model):
+	user_id = models.CharField(max_length=30)
+	name = models.CharField(max_length=30)
+	password = models.CharField(max_length=30)
